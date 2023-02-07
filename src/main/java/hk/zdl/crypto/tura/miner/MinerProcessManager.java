e9c0fa5cebcb35596d131a642a417a8989ef69d6
David Zhang <david290@qq.com>
2023-02-08 00:07:24 +0800

    restart all miners after adjust of server url

--- src/main/java/hk/zdl/crypto/tura/miner/MinerProcessManager.java
+++ src/main/java/hk/zdl/crypto/tura/miner/MinerProcessManager.java
@@ -10,43 +10,46 @@ import hk.zdl.crypto.pearlet.persistence.MyDb;
 import hk.zdl.crypto.tura.miner.util.Util;
 
 public class MinerProcessManager {
-	
+
 	public static final MinerProcessManager me = new MinerProcessManager();
 
 	private List<MinerMonitor> miners = Collections.synchronizedList(new LinkedList<MinerMonitor>());
 
+	private MinerProcessManager() {
+	}
+
 	public synchronized final void start_miner(BigInteger id) throws Exception {
-		for(var m:miners) {
-			if(m.getProperty("id").equals(id.toString())) {
-				throw new IllegalStateException("There's already a process for "+id);
+		for (var m : miners) {
+			if (m.getProperty("id").equals(id.toString())) {
+				throw new IllegalStateException("There's already a process for " + id);
 			}
 		}
 		var passphrase = MyDb.getAccount(id.toString()).get().getStr("PASSPHRASE");
 		var plot_dirs = MyDb.getMinerPaths(id.toString());
-		if(plot_dirs.isEmpty()) {
+		if (plot_dirs.isEmpty()) {
 			throw new IllegalStateException("No plot path for this wallet id.");
 		}
 		var server_url = new URL(MyDb.get_server_url().get());
 		var miner_mon = Util.buildMinerProces(id, passphrase, plot_dirs, server_url);
 		miner_mon.setProperty("id", id.toString());
-		miner_mon.setProperty("plot_dirs",plot_dirs.stream().map(o->o.toAbsolutePath().toString()).toList());
+		miner_mon.setProperty("plot_dirs", plot_dirs.stream().map(o -> o.toAbsolutePath().toString()).toList());
 		miners.add(miner_mon);
 		miner_mon.start();
 	}
-	
+
 	public synchronized final void stop_miner(BigInteger id) throws Exception {
 		var itr = miners.iterator();
-		while(itr.hasNext()) {
+		while (itr.hasNext()) {
 			var m = itr.next();
-			if(m.getProperty("id").equals(id.toString())) {
+			if (m.getProperty("id").equals(id.toString())) {
 				m.destroyForcibly();
 				itr.remove();
 				break;
 			}
 		}
 	}
-	
-	public final List<MinerMonitor> list_miners(){
+
+	public final List<MinerMonitor> list_miners() {
 		return Collections.unmodifiableList(miners);
 	}
 }
--- src/main/java/hk/zdl/crypto/tura/miner/controller/MinerConfServController.java
+++ src/main/java/hk/zdl/crypto/tura/miner/controller/MinerConfServController.java
@@ -1,11 +1,13 @@
 package hk.zdl.crypto.tura.miner.controller;
 
+import java.math.BigInteger;
 import java.net.URL;
 
 import com.jfinal.core.Controller;
 import com.jfinal.core.Path;
 
 import hk.zdl.crypto.pearlet.persistence.MyDb;
+import hk.zdl.crypto.tura.miner.MinerProcessManager;
 
 @Path(value = "/api/v1/miner/configure/server_url")
 public class MinerConfServController extends Controller {
@@ -13,12 +15,12 @@ public class MinerConfServController extends Controller {
 
 	public void index() {
 		try {
-			switch(getRequest().getMethod()) {
+			switch (getRequest().getMethod()) {
 			case "GET":
 				var o = MyDb.get_server_url();
-				if(o.isPresent()) {
+				if (o.isPresent()) {
 					renderText(o.get());
-				}else {
+				} else {
 					renderText("");
 				}
 				break;
@@ -28,9 +30,19 @@ public class MinerConfServController extends Controller {
 					new URL(line);
 				} catch (Exception e) {
 					renderError(400);
+					return;
 				}
 				MyDb.update_server_url(line);
 				renderText("ok");
+				new Thread(() -> {
+					var id_list = MinerProcessManager.me.list_miners().stream().map(x -> x.getProperty("id")).toList();
+					for (var x : id_list) {
+						try {
+							MinerProcessManager.me.start_miner(new BigInteger(x));
+						} catch (Exception e) {
+						}
+					}
+				}).start();
 				break;
 			default:
 				renderError(405);
